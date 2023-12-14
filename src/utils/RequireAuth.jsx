e2cc786d76b03323436d86fa8b43b521565d4f34
Kidus <98009847+kid-us@users.noreply.github.com>
2023-12-14 12:51:11 -0800

    Update RequireAuth.jsx

--- src/utils/RequireAuth.jsx
+++ src/utils/RequireAuth.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const RequireAuth = ({ children }) => {
   const auth = useAuth();
 
-  const key ="";
+  const key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMjU4NjkyNywianRpIjoiMDZjY2JlM2ItYzJkMy00MzE0LWIzMGEtNjFmNzIzYjVlNDA3IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImtpZHVzaGgyOUBnbWFpbC5jb20iLCJuYmYiOjE3MDI1ODY5MjcsImV4cCI6MTcwMjg0NjEyN30.12iUcxHMWAP_exA4y3vqk8fVji2CHijZir6qjbVHpHc";
   useEffect(() => {
     axios
       .get(request.getCurrentUser, {
