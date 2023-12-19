ed0d69c72984ac132493a2248b4a6ceede50b57d
Kidus <98009847+kid-us@users.noreply.github.com>
2023-12-18 23:54:09 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { AuthProvider } from "./utils/Auth";
-import RequireAuth from "./utils/RequireAuth";
+// import { AuthProvider } from "./utils/Auth";
+// import RequireAuth from "./utils/RequireAuth";
 import Betting from "./components/Betting";
 import { Route, Routes } from "react-router-dom";
 import "animate.css";
 function App() {
   return (
     <>
-      <AuthProvider>
-        <RequireAuth>
           <Routes>
             <Route path="/" exact element={<Betting></Betting>}></Route>
             <Route
@@ -31,8 +29,6 @@ function App() {
               element={<Betting></Betting>}
             ></Route> */}
           </Routes>
-        </RequireAuth>
-      </AuthProvider>
     </>
   );
 }
