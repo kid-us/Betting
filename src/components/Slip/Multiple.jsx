74c8adce6df4c9d5061e96600e08b2683989b4e9
kid-us <kidushh29@gmail.com>
2024-01-22 16:02:28 +0300

    Scroll Fixed

--- src/components/Slip/Multiple.jsx
+++ src/components/Slip/Multiple.jsx
@@ -108,7 +108,7 @@ const Form = ({ bets, balance, clearBets }) => {
                   Low Balance
                 </p>
               ) : (
-                <button className="btn btn-warning bet-btn w-100 px-1 py-2 fw-semibold mb-4 mt-1">
+                <button className="btn btn-warning bet-btn w-100 px-1 py-2 fw-semibold mb-lg-4 mt-1">
                   Place Multiple Bet
                 </button>
               )}
