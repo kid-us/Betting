a0ff35f90e000b8fb5899c91b4e73784235a3d4e
kid-us <kidushh29@gmail.com>
2024-01-24 14:26:36 +0300

    border-added

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -6,42 +6,39 @@ const MenuBar = ({ bets, onClick, onShowSport, from }) => {
     <>
       <div className="menu-bar w-100 d-block d-lg-none">
         <div className="row text-center fw-semibold pb-0 shadow-lg bg2 text-black">
-          <div className="col-3 text-warning" onClick={onShowSport}>
+          <div className="col-3" onClick={onShowSport}>
             <img src={browse} width={"25px"} alt="" />
-            <p className="fs-6">Browse</p>
+            <p className="pt-2 fw-semibold menu-bar-text">Browse</p>
           </div>
 
           {from === "my-bets" ? (
             <div className="col-3 ">
               <Link to={"/sports/home"}>
-                <img src={sports} width={"20px"} alt="icon" />
-                <p className="text-warning">Sports</p>
+                <img src={sports} width={"25px"} alt="icon" />
+                <p className="pt-2 fw-semibold menu-bar-text">Sports</p>
               </Link>
             </div>
           ) : (
-            <div
-              className="col-3 text-warning position-relative"
-              onClick={onClick}
-            >
-              <img src={slip} width={"20px"} alt="icon" />
+            <div className="col-3  position-relative" onClick={onClick}>
+              <img src={slip} width={"23px"} alt="icon" />
               <span className="position-absolute top-0 start-75 translate-middle badge rounded-pill bg-info text-black fw-semibold">
                 {bets.length}
               </span>
-              <p>Bet Slip</p>
+              <p className="pt-2 fw-semibold menu-bar-text">Bet Slip</p>
             </div>
           )}
 
           <div className="col-3 ">
             <Link to={"https://maverickhabesha.com/games.html"}>
-              <img src={game} width={"20px"} alt="icon" />
-              <p className="text-warning">Casino</p>
+              <img src={game} width={"25px"} alt="icon" />
+              <p className="pt-2 fw-semibold menu-bar-text">Casino</p>
             </Link>
           </div>
 
           <div className="col-3">
             <Link to={"https://maverickhabesha.com/vip-club.html"}>
-              <img src={vip} width={"22px"} alt="icon" />
-              <p className="text-warning">Vip</p>
+              <img src={vip} width={"25px"} alt="icon" />
+              <p className="pt-2 fw-semibold menu-bar-text">Vip</p>
             </Link>
           </div>
         </div>
